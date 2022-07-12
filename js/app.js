d50ee2352aa69b009599d6e5f957b7a299a5b542
bayanfuad <bayanjawawdeh9@gmail.com>
2022-07-12 14:52:01 +0300

    render names on html

--- js/app.js
+++ js/app.js
@@ -17,10 +17,24 @@ Employee.prototype.randomNumberSalary =function ()
         this.salary = Math.floor(Math.random() * (1000- 500 + 1) +500) 
     };
     this.salary = this.salary-this.salary*.075
+    employees.push(this.salary);
+
     // console.log(this.salary);
     return this.salary
 }
-document.write(`${this.fullName} : ${this.level}`)
+
+ 
+Employee.prototype.print = function (){
+    document.write(`${this.fullName} :${this.salary} JD <br>`)
+
+}
+
+
+// console.log(`${this.fullName} : ${this.salary} JD`)
+// for(let i=0; i<=employees.length; i++){
+//     employees[i].print
+ 
+//  document.write("anaswelcome")
 
 const employee1 = new Employee(1000,"Ghazi Samer","Administration","Senior")
 const employee2 = new Employee(1001,"Lana Ali","AFinance","Senior")
@@ -32,6 +46,14 @@ const employee7 = new Employee(1006,"Rana Saleh","Development","Junior")
 const employee8 = new Employee(1007,"Hadi Ahmad","Finance","Mid-Senior")
 
  console.log(employees);
+ employee1.print();
+ employee2.print();
+ employee3.print();
+ employee4.print();
+ employee5.print();
+ employee6.print();
+ employee7.print();
+ employee8.print();
 // console.log(employee2)
 // Employee.prototype.randomNumbeSalaryr =function (min, max)
 // {
